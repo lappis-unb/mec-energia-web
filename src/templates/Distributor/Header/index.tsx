512e016733bbb0f00a0b621ced30741fbb1c9735
DouglasMonteles <silvadouglas328@gmail.com>
2024-05-06 22:03:49 -0300

    feat: adicionando pop-up com informações dos subgrupos e tarifas

--- src/templates/Distributor/Header/index.tsx
+++ src/templates/Distributor/Header/index.tsx
@@ -3,8 +3,9 @@ import { useDispatch, useSelector } from "react-redux";
 import { skipToken } from "@reduxjs/toolkit/dist/query";
 import { useRouter } from "next/router";
 
-import { Box, Button, Collapse, Container, Typography } from "@mui/material";
+import { Box, Button, Collapse, Container, Grid, IconButton, Link, Modal, Typography } from "@mui/material";
 import EditIcon from "@mui/icons-material/Edit";
+import HelpOutlineSharpIcon from '@mui/icons-material/HelpOutlineSharp';
 
 import {
   useGetDistributorSubgroupsQuery,
@@ -23,6 +24,7 @@ import DistributorEditForm from "@/components/Distributor/Form/DistributorEditFo
 import { Delete } from "@mui/icons-material";
 import Tooltip from "@mui/material/Tooltip";
 import DeleteDistributorDialog from "@/components/Distributor/Form/DeleteDistributorDialog";
+import { getSession } from "next-auth/react";
 
 const DistributorContentHeader = () => {
   const dispatch = useDispatch();
@@ -37,6 +39,8 @@ const DistributorContentHeader = () => {
 
   const { data: tariffsSubgroups, isLoading: isTariffsSubgroupsLoading } =
     useGetDistributorSubgroupsQuery(distributorId ?? skipToken);
+
+  const [isOpenModalInfoAboutSubGroupsAndTariff, setOpenModalInfoAboutSubGroupsAndTariff] = useState(false);
 
   useEffect(() => {
     if (!tariffsSubgroups || tariffsSubgroups.length === 0) {
@@ -102,6 +106,108 @@ const DistributorContentHeader = () => {
     }
   }, [dispatch, deleteDistributor, distributorId, router]);
 
+  const ModalInfoAboutSubGroupsAndTariff = () => {
+
+    const handleDownloadClick = async (
+      event: React.MouseEvent<HTMLButtonElement>
+    ) => {
+      event?.preventDefault();
+      try {
+        const session = await getSession(); // Garanta que getSession está sendo importado corretamente.
+        const response = await fetch(
+          `${process.env.NEXT_PUBLIC_API_URL}/download-step-by-step-pdf/`,
+          {
+            method: "GET",
+            headers: {
+              Authorization: `Token ${session?.user?.token}`, // Ajuste conforme sua configuração de autenticação.
+              "Content-Type": "application/pdf",
+            },
+          }
+        );
+        if (!response.ok) throw new Error("Network response was not ok");
+        const blob = await response.blob();
+        const url = window.URL.createObjectURL(blob);
+        const link = document.createElement("a");
+        link.href = url;
+        link.setAttribute(
+          "download",
+          "Pegar_dados_de_tarifas_das_distribuidoras.pdf"
+        ); // Adapte conforme necessário
+        document.body.appendChild(link);
+        link.click();
+        link.parentNode?.removeChild(link);
+        window.URL.revokeObjectURL(url); // Limpa a referência ao objeto criado após o download.
+      } catch (error) {
+        console.error("Failed to download file:", error);
+      }
+    };
+
+    return (
+      <Modal
+        open={isOpenModalInfoAboutSubGroupsAndTariff}
+      >
+        <Grid
+        container
+        spacing={1}
+        sx={{
+          borderRadius: 1,
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          width: 600,
+          bgcolor: "background.paper",
+          boxShadow: 24,
+          p: 4,
+          "@media print": {
+            borderRadius: 1,
+            width: 600,
+            bgcolor: "background.paper",
+            boxShadow: 24,
+            p: 4,
+          },
+        }}
+      >
+        <Grid item marginBottom={2}>
+          <Typography variant="h6">Subgrupos e Tarifas</Typography>
+        </Grid>
+        <Grid item>
+          <Typography variant="body1">
+            Os subgrupos correspondem à tensão de fornecimento contratada para 
+            uma unidade Consumidora. Apenas os subgrupos usados em unidades 
+            consumidoras ativas precisam estar atualizadas.
+          </Typography>
+        </Grid>
+        <Grid item>
+          <Typography variant="body1">
+            A vigência e os valores das tarifas são determinadas pela ANEEL. Os
+            valores podem variar de um subgrupo a outro.
+          </Typography>
+        </Grid>
+        <Grid item>
+          <Typography variant="body1">
+            Siga <Link 
+              style={{cursor: "pointer"}}
+              fontWeight="bold" 
+              onClick={handleDownloadClick}
+              target="_blank"
+            >este passo-a-passo</Link> para encontrar as informações de tarifa no site da ANEEL.
+          </Typography>
+        </Grid>
+
+        <Grid item width="100%" display="flex" justifyContent="end">
+          <Button 
+            onClick={() => setOpenModalInfoAboutSubGroupsAndTariff(false)} 
+            variant="text"
+          >
+            Fechar
+          </Button>
+        </Grid>
+      </Grid>
+      </Modal>
+    );
+  };
+
   return (
     <Box
       position="sticky"
@@ -110,7 +216,7 @@ const DistributorContentHeader = () => {
       sx={{ backgroundColor: "background.default" }}
     >
       <Container>
-        <Box display="flex">
+        <Box display="flex" justifyContent="space-between">
           <Box>
             <Box display="flex" alignItems="center">
               <Typography variant="h4">{distributor?.name}</Typography>
@@ -172,6 +278,14 @@ const DistributorContentHeader = () => {
               CNPJ: <strong>{distributor?.cnpj}</strong>
             </Typography>
           </Box>
+
+          <Box>
+            <IconButton onClick={() => setOpenModalInfoAboutSubGroupsAndTariff(true)}>
+              <HelpOutlineSharpIcon />
+            </IconButton>
+          </Box>
+
+          <ModalInfoAboutSubGroupsAndTariff />
         </Box>
 
         <Collapse in={shouldShowTabs}>
