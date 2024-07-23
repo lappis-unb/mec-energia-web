0aa77830a391222572d93904e62b6ca15d38f6a4
Flavio Vieira Leao <flavio.vl@gmail.com>
2024-07-23 19:26:40 -0300

    refactor: utualiza configuracao do docker para o ambiente de producao

--- next.config.js
+++ next.config.js
@@ -1,19 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // https://github.com/chartjs/Chart.js/issues/10673#issuecomment-1248543069
-  // Desligar o minify resolve o bug do chartjs em build de produção
-  // FIXME: a gente tem que achar outro workaround
   swcMinify: false,
-  serverRuntimeConfig:{
-    API_URL: process.env.API_URL
-  },
-  publicRuntimeConfig:{
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  typescript: { ignoreBuildErrors: true,},
+  output: 'standalone'
 };
 
 module.exports = nextConfig;
