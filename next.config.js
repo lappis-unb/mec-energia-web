31be006fdcd7687120f4f8ce5242c7eb0f0e9c90
Flavio Vieira Leao <flavio.vl@gmail.com>
2024-07-24 14:19:33 -0300

    chore: reverte comentario deletado

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+    // https://github.com/chartjs/Chart.js/issues/10673#issuecomment-1248543069
+  // Desligar o minify resolve o bug do chartjs em build de produção
+  // FIXME: a gente tem que achar outro workaround
   swcMinify: false,
   typescript: { ignoreBuildErrors: true,},
   output: 'standalone'
