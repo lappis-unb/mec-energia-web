c101fa4536e543695871e32d215951193ca87273
Enzo <eggqsaraiva@gmail.com>
2024-07-31 01:12:30 -0300

    fix 369: Redirecionamento para a página de login em caso de não estar autenticado

--- src/pages/instituicoes.tsx
+++ src/pages/instituicoes.tsx
@@ -4,8 +4,25 @@ import InstitutionsTemplate from "@/templates/Institution";
 import InstitutionHeaderAction from "@/templates/Institution/HeaderAction";
 import SuccessNotification from "@/components/Notification/SuccessNotification";
 import FailNotification from "@/components/Notification/FailNotification";
+import { useRouter } from "next/router";
+import { useSession } from "next-auth/react";
+import { UserRole } from "@/types/person";
+import { useEffect } from "react";
 
 const InstitutionsPage: NextPage = () => {
+  const { data: session, status } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (status === "unauthenticated" || session?.user.type !== UserRole.SUPER_USER) {
+      router.push("/");
+    }
+  }, [status, session, router]);
+
+  if (status === "unauthenticated" || session?.user.type !== UserRole.SUPER_USER) {
+    return null;
+  }
+
   return (
     <DefaultTemplateV2 headerAction={<InstitutionHeaderAction />}>
       <InstitutionsTemplate />
--- src/pages/pessoas.tsx
+++ src/pages/pessoas.tsx
@@ -4,13 +4,25 @@ import UserListTemplate from "@/templates/UserList";
 import SuccessNotification from "@/components/Notification/SuccessNotification";
 import FailNotification from "@/components/Notification/FailNotification";
 import CreatePersonHeaderAction from "@/templates/UserList/CreatePersonHeaderAction";
+import { useRouter } from "next/router";
+import { useSession } from "next-auth/react";
 
-const UserListPage: NextPage = () => (
-  <DefaultTemplateV2 headerAction={<CreatePersonHeaderAction />}>
-    <UserListTemplate />
-    <SuccessNotification />
-    <FailNotification />
-  </DefaultTemplateV2>
-);
+const UserListPage: NextPage = () => {
+  const { status } = useSession();
+  const router = useRouter();
+
+  if (status === "unauthenticated") {
+    router.push("/");
+    return null;
+  }
+
+  return (
+    <DefaultTemplateV2 headerAction={<CreatePersonHeaderAction />}>
+      <UserListTemplate />
+      <SuccessNotification />
+      <FailNotification />
+    </DefaultTemplateV2>
+  )
+};
 
 export default UserListPage;
