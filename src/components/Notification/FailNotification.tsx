08a200d8e0598bc641fdd9d64069b74af7c73f4e
Julio Cesar <julio0033@hotmail.com>
2024-09-02 13:53:29 -0300

    fix: #sprint5 Eliminando Duplicação de Código Para os Componentes de Notificação

--- src/components/Notification/FailNotification.tsx
+++ src/components/Notification/FailNotification.tsx
@@ -2,37 +2,15 @@ import {
   selectErrorNotification,
   setIsErrorNotificationOpen,
 } from "@/store/appSlice";
-import { Alert, Snackbar } from "@mui/material";
-import { useDispatch, useSelector } from "react-redux";
+import Notification from "./Notification";
 
 const FailNotification = () => {
-  const dispatch = useDispatch();
-  const notification = useSelector(selectErrorNotification);
-  const handleClose = (
-    event?: React.SyntheticEvent | Event,
-    reason?: string
-  ) => {
-    if (reason === "clickaway") {
-      return;
-    }
-    dispatch(
-      setIsErrorNotificationOpen({
-        isOpen: false,
-      })
-    );
-  };
-
   return (
-    <Snackbar
-      open={notification.isOpen}
-      autoHideDuration={6000}
-      onClose={handleClose}
-      anchorOrigin={{ horizontal: "center", vertical: "top" }}
-    >
-      <Alert onClose={handleClose} severity="error" sx={{ width: "100%" }}>
-        {notification.text}
-      </Alert>
-    </Snackbar>
+    <Notification
+      selectNotification={selectErrorNotification}
+      setIsNotificationOpen={setIsErrorNotificationOpen}
+      severity="error"
+    />
   );
 };
 export default FailNotification;
