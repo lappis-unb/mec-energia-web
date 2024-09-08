e6053083c6f6dae375fdb10dcf0959ce15fafcdd
João Victor Correia <correia.jv16@gmail.com>
2024-09-08 23:34:40 +0000

    275/bloquear datas

--- src/components/ElectricityBill/Form/CreateEditElectricityBillForm.tsx
+++ src/components/ElectricityBill/Form/CreateEditElectricityBillForm.tsx
@@ -43,10 +43,11 @@ import {
   useGetCurrentInvoiceQuery,
   useGetDistributorsQuery,
   usePostInvoiceMutation,
-  useFetchInvoicesQuery
+  useFetchInvoicesQuery,
 } from "@/api";
 import { skipToken } from "@reduxjs/toolkit/dist/query";
 import { useSession } from "next-auth/react";
+import FormFieldError from "../../FormFieldError";
 import { DistributorPropsTariffs } from "@/types/distributor";
 import { sendFormattedDate } from "@/utils/date";
 import FormDrawerV2 from "@/components/Form/DrawerV2";
@@ -131,35 +132,63 @@ const CreateEditEnergyBillForm = () => {
   const offPeakConsumptionInKwh = watch("offPeakConsumptionInKwh");
   const peakMeasuredDemandInKw = watch("peakMeasuredDemandInKw");
   const offPeakMeasuredDemandInKw = watch("offPeakMeasuredDemandInKw");
+  const [currentInvoiceYear, currentInvoiceMonth] = String(currentInvoice?.date)
+    .split("-")
+    .map(Number);
 
-  const updateCurrentInvoiceData = useCallback((
-    currentInvoice: CurrentEnergyBillResponsePayload | undefined = undefined
-  ) => {
-    setValue(
-      "invoiceInReais",
-      currentInvoice?.invoiceInReais?.toString() ?? ""
-    );
-    setValue(
-      "peakConsumptionInKwh",
-      currentInvoice?.peakConsumptionInKwh ?? ""
-    );
-    setValue(
-      "offPeakConsumptionInKwh",
-      currentInvoice?.offPeakConsumptionInKwh ?? ""
-    );
-    setValue(
-      "peakMeasuredDemandInKw",
-      currentInvoice?.peakMeasuredDemandInKw ?? ""
-    );
-    setValue(
-      "offPeakMeasuredDemandInKw",
-      currentInvoice?.offPeakMeasuredDemandInKw ?? ""
-    );
-  }, [setValue]);
+  const findMinContractDate = () => {
+    if (contracts && contracts.length > 0) {
+      const earliestContract = contracts.reduce((earliest, current) => {
+        return new Date(current.startDate) < new Date(earliest.startDate)
+          ? current
+          : earliest;
+      });
+      const dateParts = earliestContract.startDate.split("-");
+      return new Date(
+        parseInt(dateParts[0], 10),
+        parseInt(dateParts[1], 10) - 1,
+        parseInt(dateParts[2], 10)
+      );
+    }
+    return new Date("2009-01-01");
+  };
+
+  const blockedYears = (datePickerElement: Date) => {
+    const year = datePickerElement.getFullYear();
+    const minContractYear = findMinContractDate().getFullYear();
+    if (year < minContractYear) {
+      return true;
+    }
+
+    return false;
+  };
+
+  const minContractYear = findMinContractDate().getFullYear();
+  const blockedMonths = (datePickerElement: Date) => {
+    const year = datePickerElement.getFullYear();
+    const month = datePickerElement.getMonth();
+    const minContractMonth = findMinContractDate().getMonth();
+
+    if (year == minContractYear && month < minContractMonth) {
+      return true;
+    }
+
+    if (invoices && invoices[year]) {
+      const monthData = invoices[year].find((entry) => entry.month === month);
+      if (
+        monthData &&
+        monthData.energyBill &&
+        (year !== currentInvoiceYear || month + 1 !== currentInvoiceMonth)
+      ) {
+        return true;
+      }
+    }
+    return false;
+  };
 
   useEffect(() => {
     reset();
-  }, [isCreateEnergyBillFormOpen, reset]);
+  }, [isCreateEnergyBillFormOpen]);
 
   useEffect(() => {
     if (isEditEnergyBillFormOpen) {
@@ -177,11 +206,14 @@ const CreateEditEnergyBillForm = () => {
         fetchData();
       }
     }
-  }, [isEditEnergyBillFormOpen, refetchCurrentInvoice, updateCurrentInvoiceData]);
+  }, [isEditEnergyBillFormOpen]);
 
   useEffect(() => {
-    if (month != null || month != undefined) {
-      const date = new Date(`${year}/${month + 1}`);
+    if (
+      (month != null || month != undefined) &&
+      (year != null || year != undefined)
+    ) {
+      const date = new Date(year, month, 1);
       setValue("date", date);
     }
   }, [
@@ -206,7 +238,6 @@ const CreateEditEnergyBillForm = () => {
     isCreateEnergyBillFormOpen,
     isEditEnergyBillFormOpen,
     setValue,
-    updateCurrentInvoiceData,
   ]);
 
   useEffect(() => {
@@ -248,6 +279,31 @@ const CreateEditEnergyBillForm = () => {
     );
     if (distributor) setCurrentDistributor(distributor);
   }, [contract?.distributor, distributors]);
+
+  const updateCurrentInvoiceData = (
+    currentInvoice: CurrentEnergyBillResponsePayload | undefined = undefined
+  ) => {
+    setValue(
+      "invoiceInReais",
+      currentInvoice?.invoiceInReais?.toString() ?? ""
+    );
+    setValue(
+      "peakConsumptionInKwh",
+      currentInvoice?.peakConsumptionInKwh ?? ""
+    );
+    setValue(
+      "offPeakConsumptionInKwh",
+      currentInvoice?.offPeakConsumptionInKwh ?? ""
+    );
+    setValue(
+      "peakMeasuredDemandInKw",
+      currentInvoice?.peakMeasuredDemandInKw ?? ""
+    );
+    setValue(
+      "offPeakMeasuredDemandInKw",
+      currentInvoice?.offPeakMeasuredDemandInKw ?? ""
+    );
+  };
 
   const handleCancelEdition = () => {
     if (isDirty) {
@@ -370,19 +426,20 @@ const CreateEditEnergyBillForm = () => {
     handleNotification();
   }, [handleNotification, isPostInvoiceSuccess, isPostInvoiceError]);
 
-  const checkIfInvoiceExists = useCallback((year: number, month: number): boolean => {
-    if (isEditEnergyBillFormOpen) {
-      return false;
-    }
+  const checkIfInvoiceExists = (
+    selectedYear: number,
+    selectedMonth: number
+  ): boolean => {
+    if (selectedMonth == month && selectedYear == year) return false;
 
-    if (invoices && year in invoices) {
-      return invoices[year].some(
+    if (invoices && selectedYear in invoices) {
+      return invoices[selectedYear].some(
         (invoice) =>
-          invoice.month === month && !invoice.isEnergyBillPending
+          invoice.month === selectedMonth && invoice.energyBill !== null
       );
     }
     return false;
-  }, [invoices, isEditEnergyBillFormOpen]);
+  };
 
   const Header = useCallback(
     () => (
@@ -399,7 +456,9 @@ const CreateEditEnergyBillForm = () => {
     () => (
       <>
         <Grid item xs={8}>
-          <Typography variant="h5">Fatura</Typography>
+          <Typography variant="h5" style={{ marginBottom: "13px" }}>
+            Fatura
+          </Typography>
         </Grid>
         <Grid item xs={12} mt={1}>
           <Controller
@@ -439,13 +498,18 @@ const CreateEditEnergyBillForm = () => {
                     earliestContract.startDate
                   );
 
-                  const contractStartDateMonth = (contractStartDate.getMonth())
-                  const contractStartDateYear = (contractStartDate.getFullYear())
+                  const contractStartDateMonth = contractStartDate.getMonth();
+                  const contractStartDateYear = contractStartDate.getFullYear();
 
-                  const fixedDate = new Date(`${contractStartDateYear}/${contractStartDateMonth + 2}`);
+                  const fixedDate = new Date(
+                    `${contractStartDateYear}/${contractStartDateMonth + 2}/01`
+                  );
 
-                  const options = { year: 'numeric', month: 'long' };
-                  const formattedDate = fixedDate.toLocaleDateString('pt-BR', options);
+                  const options = { year: "numeric", month: "long" };
+                  const formattedDate = fixedDate.toLocaleDateString(
+                    "pt-BR",
+                    options
+                  );
 
                   const message = `Selecione uma data a partir de ${formattedDate}. Não existem contratos registrados antes disso.`;
 
@@ -461,15 +525,20 @@ const CreateEditEnergyBillForm = () => {
                 <DatePicker
                   inputFormat="MMMM/yyyy"
                   value={value}
+                  views={["month", "year"]}
                   label="Mês de referência *"
-                  minDate={new Date("2010")}
-                  disableFuture
+                  minDate={new Date(minContractYear, 0)}
+                  maxDate={new Date()}
+                  shouldDisableMonth={blockedMonths}
+                  shouldDisableYear={blockedYears}
                   renderInput={(params) => (
                     <TextField
                       {...params}
                       inputProps={{
                         ...params.inputProps,
-                        placeholder: "mm/aaaa",
+                        placeholder: "mês/aaaa",
+                        readOnly: true,
+                        style: { userSelect: "none" },
                       }}
                       error={!!error}
                       helperText={
@@ -486,6 +555,16 @@ const CreateEditEnergyBillForm = () => {
                     />
                   )}
                   onChange={onChange}
+                  PopperProps={{
+                    sx: {
+                      "& .Mui-disabled": {
+                        textDecoration: "line-through",
+                      },
+                      "& .PrivatePickersMonth-root[disabled]": {
+                        textDecoration: "line-through",
+                      },
+                    },
+                  }}
                 />
               </Box>
             )}
@@ -588,10 +667,8 @@ const CreateEditEnergyBillForm = () => {
                       </Box>
                     )}
                     <FormHelperText>
-                      <p>
-                        Inclua todas as faturas, exceto casos radicalmente
-                        excepcionais como greves ou a pandemia
-                      </p>
+                      Inclua todas as faturas, exceto casos radicalmente
+                      excepcionais como greves ou a pandemia
                     </FormHelperText>
                   </Box>
                 </FormGroup>
@@ -606,7 +683,6 @@ const CreateEditEnergyBillForm = () => {
       currentInvoice,
       isCreateEnergyBillFormOpen,
       isEditEnergyBillFormOpen,
-      checkIfInvoiceExists,
       contracts,
     ]
   );
@@ -625,7 +701,7 @@ const CreateEditEnergyBillForm = () => {
               name="peakMeasuredDemandInKw"
               rules={{
                 validate: (value) => {
-                  if (contract?.tariffFlag !== 'G' && !value) {
+                  if (contract?.tariffFlag !== "G" && !value) {
                     return "Preencha este campo";
                   }
                 },
@@ -641,7 +717,7 @@ const CreateEditEnergyBillForm = () => {
                 <NumericFormat
                   value={value}
                   customInput={TextField}
-                  label={contract?.tariffFlag === 'G' ? 'Ponta' : 'Ponta *'}
+                  label={contract?.tariffFlag === "G" ? "Ponta" : "Ponta *"}
                   fullWidth
                   InputProps={{
                     endAdornment: (
@@ -658,7 +734,10 @@ const CreateEditEnergyBillForm = () => {
                   decimalSeparator=","
                   thousandSeparator={"."}
                   error={Boolean(error)}
-                  helperText={error?.message ?? (contract?.tariffFlag !== 'G' ? "" : "Campo opcional")}
+                  helperText={
+                    error?.message ??
+                    (contract?.tariffFlag !== "G" ? "" : "Campo opcional")
+                  }
                   onValueChange={(values) => onChange(values.floatValue)}
                   onBlur={onBlur}
                 />
@@ -710,17 +789,16 @@ const CreateEditEnergyBillForm = () => {
         </Grid>
       </>
     ),
-    [
-      control,
-      contract?.tariffFlag,
-    ]
+    [control, activeConsumerUnitId, contract?.tariffFlag]
   );
 
   const MeasuredConsumption = useCallback(
     () => (
       <>
         <Grid item xs={10}>
-          <Typography variant="h5">Consumo medido</Typography>
+          <Typography variant="h5" style={{ marginBottom: "16px" }}>
+            Consumo medido
+          </Typography>
         </Grid>
 
         <Grid container spacing={2}>
@@ -759,7 +837,7 @@ const CreateEditEnergyBillForm = () => {
                   decimalSeparator=","
                   thousandSeparator={"."}
                   error={Boolean(error)}
-                  helperText={error?.message ?? " "}
+                  helperText={FormFieldError(error?.message)}
                   onValueChange={(values) => onChange(values.floatValue)}
                   onBlur={onBlur}
                 />
@@ -801,7 +879,7 @@ const CreateEditEnergyBillForm = () => {
                   decimalSeparator=","
                   thousandSeparator={"."}
                   error={Boolean(error)}
-                  helperText={error?.message ?? " "}
+                  helperText={FormFieldError(error?.message)}
                   onValueChange={(values) => onChange(values.floatValue)}
                   onBlur={onBlur}
                 />
